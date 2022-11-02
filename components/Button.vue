b9365944e02129e68f27c9d933dd04a632399182
dewangga-pk <pacetcommunity570@gmail.com>
2022-11-02 22:57:13 +0700

    init project & two default button

---
+++ components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button
+      type="button"
+      class="w-[68px] h-[68px] rounded-full flex items-center justify-center cursor-pointer focus:shadow-[-12px_0_rgba(0,0,0,0.3)]"
+    >
+      <img src="@/assets/stroke-icon.png" alt="quicks" class="w-4 h-8">
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SpButton'
+}
+</script>
